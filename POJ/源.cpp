cb83e7d6a09f232ce9b41a07108e8ef8af3c5663
叶志晟 <yzs981130@126.com>
2016-04-29 00:36:26 +0800

    Communicatio System

--- POJ/源.cpp
+++ POJ/源.cpp
@@ -1,34 +1,59 @@
-/*02:Integer Inquiry
+/*03:Communication System
  *程序设计实习 / 2015程序设计实习之上机练习题
- *http://cxsjsx.openjudge.cn/2015finalpractice/02/
+ *http://cxsjsx.openjudge.cn/2015finalpractice/03/
  */
-#include <iostream>
-#include <string>
-using namespace std;
-int ans[102] = {0}, tmp[102];
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 101
+
 int main()
 {
-    string str;
-    while (cin >> str && str != "0")
+    int i, j;
+    int t, n, m[N], k;
+    int band[N][N], price[N][N], low, high, sump, minp;
+    float ratio;
+    scanf("%d", &t);
+    while (t--)
     {
-        for (int i = 0; i < str.length(); i++)
-            tmp[i] = str[str.length() - i - 1] - '0';
-        for (int i = 0; i < 101; i++)
+        scanf("%d", &n);
+        low = 0xffff;
+        high = 0;
+        for (i = 1; i <= n; i++)
         {
-            ans[i] += tmp[i];
-            if (ans[i] > 9)
+            scanf("%d", &m[i]);
+            for (j = 1; j <= m[i]; j++)
             {
-                ans[i] -= 10;
-                ans[i + 1]++;
+                scanf("%d%d", &band[i][j], &price[i][j]);
+                if (band[i][j] < low)
+                    low = band[i][j];
+                if (band[i][j] > high)
+                    high = band[i][j];
             }
         }
+        ratio = 0.0;
+        for (i = low; i <= high; i++)
+
+        {
+            sump = 0;
+            for (j = 1; j <= n; j++)
+            {
+                minp = 0xffff;
+                for (k = 1; k <= m[j]; k++)
+
+                    if (band[j][k] >= i && price[j][k] < minp)
+                    {
+                        minp = price[j][k];
+                    }
+                sump += minp;
+
+            }
+
+            if (i * 1.0 / sump - ratio > 0)
+                ratio = i * 1.0 / sump;
+        }
+        printf("%.3f\n", ratio);
     }
-    int pos = 101;
-    while (ans[pos] == 0)
-        pos--;
-    for (int i = pos; i >= 0; i--)
-        cout << ans[i];
-    cout << endl;
     system("pause");
     return 0;
 }
