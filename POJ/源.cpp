aa53eeb53895086e9742e6c2093c134497e4d9b4
叶志晟 <yzs981130@126.com>
2016-05-18 11:57:18 +0800

    POJ1458

--- POJ/源.cpp
+++ POJ/源.cpp
@@ -1,17 +1,37 @@
-/*04:判断闰年
- *程序设计实习 / 2015程序设计实习之上机练习题
- *http://cxsjsx.openjudge.cn/2015finalpractice/04/
+/*1458:Common Subsequence
+ *百练
+ *http://bailian.openjudge.cn/practice/1458/
  */
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstring>
 using namespace std;
 int main()
 {
-    int n;
-    cin >> n;
-    if (n % 4 == 0 && n % 100 != 0 || n % 400 == 0)
-        cout << 'Y' << endl;
-    else
-        cout << 'N' << endl;
+    char a[210], b[210];
+    while (scanf("%s%s", a + 1, b + 1) != EOF)
+    {
+        int table[210][210], i, j;
+        int lena = strlen(a + 1);
+        int lenb = strlen(b + 1);
+        for (i = 0; i <= lena; i++)
+            table[i][0] = 0;
+        for (i = 0; i <= lenb; i++)
+            table[0][i] = 0;
+        for (i = 1; i <= lena; i++)
+        {
+            for (j = 1; j <= lenb; j++)
+            {
+                if (a[i] == b[j])
+                    table[i][j] = table[i - 1][j - 1] + 1;
+                else if (table[i - 1][j] > table[i][j - 1])
+                    table[i][j] = table[i - 1][j];
+                else
+                    table[i][j] = table[i][j - 1];
+            }
+        }
+        printf("%d\n", table[lena][lenb]);
+    }
     system("pause");
     return 0;
 }
